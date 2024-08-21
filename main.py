eea15bf0ece4dbe64c82ddc618271d3975173ca9
Alfred Kållberg <alfred.kallberg@elev.ga.ntig.se>
2024-08-21 11:06:50 +0200

    klar

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 # Dice sum probability calculator
-# Författare: 
-# Datum:
+# Författare: Alfred Kållberg
+# Datum: 21-08-2024
 
 def main():
-    user_input = input().split(" ")
+
+    user_input = input()
+    first_die = int(user_input.split(" ")[0])
+    second_die = int(user_input.split(" ")[1])
+    sums = {}
+
+    for i in range(1, first_die + 1):
+        for j in range(1, second_die +1):
+            if i+j in sums:
+                sums[i+j] += 1
+            else:
+                sums[i+j] = 1
+    max_value = max(sums.values())
+    for i in sums:
+        if sums[i] == max_value:
+            print(i)
 
 if __name__ == "__main__":
     main()
